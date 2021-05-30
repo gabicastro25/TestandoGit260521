f5dc84bb8a61a22d24152761f87b4a67e6edee5c
Gabriella Castro <gabicastro.pdi@gmail.com>
2021-05-30 15:46:00 -0300

    Initial commit

---
+++ app/src/main/java/com/example/testandogit260521/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.testandogit260521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Gabriella de Castro Rodrigues
+    }
+}
